739324cbcbab1656c9f645fe387da9f716d28033
Jhoni <jhoni.jlm97@gmail.com>
2021-11-13 16:08:07 -0500

    patch

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,12 +2,23 @@ import vue from 'rollup-plugin-vue'
 import buble from 'rollup-plugin-buble'
 
 import styles from "rollup-plugin-styles";
+
+// import externalGlobals from "rollup-plugin-external-globals";
 export default {
     input: 'src/index.js',
+    external: [
+        'lodash/debounce',
+        'vue',
+        'bootstrap/dist/css/bootstrap.min.css'
+    ],
     output: {
         name: 'JlDatatable',
         exports: 'named',
         assetFileNames: "[name]-[hash][extname]",
+        globals : {
+            'lodash/debounce': 'debounce',
+            'vue': 'vue'
+        },
     },
     plugins: [
         vue({
@@ -18,6 +29,7 @@ export default {
             },
         }),
         buble({transforms: { forOf: false, asyncAwait: false }}),
-        styles()
+        styles(),
+        // externalGlobals({})
     ],
 }
--- src/JlDatatable.vue
+++ src/JlDatatable.vue
@@ -348,3 +348,5 @@ export default {
   }
 }
 </script>
+
+
